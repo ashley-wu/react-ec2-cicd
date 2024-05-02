a5569406f32951d9273eb0aa070222e1b64bc2b1
Ashley Wu <ashley@lustertech.net>
2024-05-02 19:22:02 +0800

    Add Page Title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React／AWS EC2／Github Action
+          React／AWS EC2／Github Action Project
         </a>
       </header>
     </div>
